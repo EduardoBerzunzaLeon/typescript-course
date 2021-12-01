c0d5618091fde84096c5b57c445de04ef7198da5
unknown <eduardoberzunzal@gmail.com>
2021-11-30 19:02:47 -0600

    do a final test

--- 01-bases/tipos/null-undefined.ts
+++ 01-bases/tipos/null-undefined.ts
@@ -1,6 +1,6 @@
 (() => {
   //   let nothing: undefined = undefined;
-  //   let isActive: boolean | undefined = undefined;
+  let isActive: boolean | undefined = undefined;
   //   let isActive: boolean = undefined; // funciona si se quita del tsconfig el strictnullcheck a false, no es recomendable
 
   console.log(isActive);
